f9e4383c0e56b12d4654fd8e6496dad4a58ba135
brunopenela <104132883+brunopenela@users.noreply.github.com>
2022-11-23 10:17:48 +0000

    base file

---
+++ mambu_rest_api.py
@@ -0,0 +1,17 @@
+#Asset REST API MAMBU
+
+#Conecção a buckets S3
+
+#Extrair Configfile.json
+
+#Ler Configfile.json
+
+#Colocar ficheiros do bucket S3
+
+#Conecção a MAMBU
+
+#Ver Dados de Mambu
+
+#.........
+#.........
+
